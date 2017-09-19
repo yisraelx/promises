406e14c297461dec66fe73ceabeb6820385daa0c
yisraelx <yisraelx@gmail.com>
2017-09-20 01:13:52 +0300

    build(scripts): improved/fixed of the update package.json script

--- scripts/update-package-json.ts
+++ scripts/update-package-json.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import { join } from 'path';
 import * as depcheck from 'depcheck';
 import * as _ from 'lodash';
-import getConfig, { PACKAGE_TYPES } from './utils/config';
+import getConfig from './utils/config';
 
 const CONFIG = getConfig();
 const PACKAGE_JSON_ROOT = require(join(CONFIG.rootPath, 'package.json'));
@@ -10,54 +10,66 @@ const PACKAGE_JSON_PATH = join(CONFIG.packagePath, 'package.json');
 const PACKAGE_JSON = require(PACKAGE_JSON_PATH);
 const PACKAGE_CONSTANTS_PROPERTIES = ['author', 'license', 'keywords', 'homepage', 'repository', 'bugs'];
 
-const DEPENDENCIES = ['@promises/interfaces'];
-const VERSION = '0.0.1-PLACEHOLDER';
+const IGNORE_DEPENDENCIES = ['@promises/interfaces'];
 
-Promise.all([
-    getConstants(CONFIG, PACKAGE_JSON_ROOT),
-    getDependencies(CONFIG, PACKAGE_JSON_ROOT)
-]).then((result) => {
-    result.forEach((updateProps) => {
-        _.forEach(updateProps, (value, key) => {
-            if (value == null)return;
-            PACKAGE_JSON[key] = value;
-        });
+let step = Promise.resolve();
+
+_.forEach([
+    getConstants,
+    updateExternalDependencies,
+    checkAndRemoveDependencies
+], (fn) => {
+    step = step.then(() => {
+        return fn();
     });
+});
+
+step.then(() => {
     return savePackage(PACKAGE_JSON_PATH, PACKAGE_JSON);
 });
+
+function getConstants() {
+    let constants = _.pick(PACKAGE_JSON_ROOT, PACKAGE_CONSTANTS_PROPERTIES);
+    _.merge(PACKAGE_JSON, constants);
+}
+
+function updateExternalDependencies() {
+    _.forEach([
+        'dependencies',
+        'devDependencies',
+        'optionalDependencies',
+        'peerDependencies'
+    ], type => {
+        let dependencies = PACKAGE_JSON[type];
+        if (dependencies) {
+            _.forEach(dependencies, (version, name) => {
+                let rootVersion = _.get(PACKAGE_JSON_ROOT, `${type}.${name}`);
+                if (rootVersion) _.set(dependencies, name, rootVersion);
+            });
+        }
+    });
+}
+
+function checkAndRemoveDependencies() {
+    let {packagePath, packageName} = CONFIG;
+    return depcheck(packagePath, {json: true, ignoreMatches: IGNORE_DEPENDENCIES}).then(result => {
+        _.unset(PACKAGE_JSON, result.dependencies || []);
+        _.unset(PACKAGE_JSON, result.devDependencies || []);
+        if (result.missing.length) {
+            console.log(`Missing dependencies in package ${packageName}:\n${JSON.stringify(result.missing, null, 1)}`);
+            process.exit(1);
+        }
+    });
+}
 
 
 function savePackage(packageJsonPath: string, packageObject: { [key: string]: any }) {
     let packageString = JSON.stringify(packageObject, null, 2);
 
     return new Promise((resolve, reject) => {
-        fs.writeFile(packageJsonPath, packageString, (error) => {
+        fs.writeFile(packageJsonPath, `${packageString}\n`, (error) => {
             if (error) return reject(error);
             resolve();
-        });
-    });
-}
-
-function getConstants(config, packageJsonRoot) {
-    return _.pick(packageJsonRoot, PACKAGE_CONSTANTS_PROPERTIES);
-}
-
-function getDependencies({packagePath, packageName}, packageJsonRoot) {
-    return getUsingDependencies(packagePath).then((dependencies: string[]) => {
-        return DEPENDENCIES.filter(name => name !== packageName).concat(dependencies).sort().reduce((result, name) => {
-            let type = (_.get(packageJsonRoot, `optionalDependencies.${name}`) && 'optionalDependencies') || (_.get(packageJsonRoot, `peerDependencies.${name}`) && 'peerDependencies') || 'dependencies';
-            let version = _.get(packageJsonRoot, `${type}.${name}`) || VERSION;
-            _.set(result, `${type}.${name}`, version);
-            return result;
-        }, {});
-    });
-}
-
-function getUsingDependencies(packagePath: string) {
-    return new Promise<string[]>((resolve) => {
-        depcheck(packagePath, {json: true}, (result) => {
-            let dependencies = Object.keys(result.using);
-            resolve(dependencies);
         });
     });
 }
